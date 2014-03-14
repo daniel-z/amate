8b1903bf70495ac069421650c706bbe237ecf8a5
Daniel Zamorano <daniel.zamorano@wisertogether.com>
2014-03-14 16:25:58 -0600

    Home page done.

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,19 @@
 $(function() {
-  $.vegas({
-    src: ['/images/home-slider/01.jpg', '/images/home-slider/01.jpg']
-  })('overlay', {
-    // src:'/vegas/overlays/13.png'
-  });
+  $.vegas('slideshow', {
+    delay:8000,
+    backgrounds:[
+      { src:'/images/home-slider/04.jpg', fade:2000 },
+      { src:'/images/home-slider/11.jpg', fade:2000 },
+      { src:'/images/home-slider/05.jpg', fade:2000 },
+      { src:'/images/home-slider/01.jpg', fade:2000 },
+      { src:'/images/home-slider/02.jpg', fade:2000 },
+      { src:'/images/home-slider/03.jpg', fade:2000 },
+      { src:'/images/home-slider/06.jpg', fade:2000 },
+      { src:'/images/home-slider/07.jpg', fade:2000 },
+      { src:'/images/home-slider/08.jpg', fade:2000 },
+      { src:'/images/home-slider/09.jpg', fade:2000 },
+      { src:'/images/home-slider/10.jpg', fade:2000 }
+    ]
+  })('overlay');
 });
+
